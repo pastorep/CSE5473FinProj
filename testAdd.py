79bb469433c8c2cee827884bdb7ed72514045349
root <root@localhost.localdomain>
2018-04-14 12:03:46 -0400

    redirect successful

--- testAdd.py
+++ testAdd.py
@@ -3,26 +3,33 @@ from mitmproxy.models import HTTPResponse
 from netlib.http import Headers
 
 def request(flow):
-     print('FLAG')
-     print(flow.request.pretty_host)
-     if flow.request.pretty_host == "google.com":
-         #flow.request.host = "10.0.2.4"
-         flow.request.host = "whipped.in"
-         #flow.request.port = "5000"
+	print('FLAG')
+	print(flow.request.pretty_host)
+	print(flow.client_conn.address)
+	addr = flow.client_conn.address
+
+	if flow.request.pretty_host is not '':
+		flow.request.host = "localhost"
+		#flow.request.host = "whipped.in"
+		flow.request.port = 5000
+		flow.request.scheme = 'http'
+		print('MODIFIED')
+		print(flow.request.host)
 
 
-#    print("CATCHINIT!")
-#    print(flow.client_conn.address)
-#    flow.request.host = "10.0.2.4"
-#    flow.request.port = "5000"
-#    flow.request.scheme = 'http'    
-#    flow.request.headers["Host"] = "10.0.2.4:5000"
-#    flow.reply(HTTPResponse('HTTP/1.1', 302, 'Found', Headers(Location='http://#stackoverflow.com/',Content_Length='0'),b''))
+
+#	print("CATCHINIT!")
+#	print(flow.client_conn.address)
+#	flow.request.host = "10.0.2.4"
+#	flow.request.port = "5000"
+#	flow.request.scheme = 'http'    
+#	flow.request.headers["Host"] = "10.0.2.4:5000"
+#	flow.reply(HTTPResponse('HTTP/1.1', 302, 'Found', Headers(Location='http://#stackoverflow.com/',Content_Length='0'),b''))
 
 
-    #method = flow.request.path.split('/')[3].split('?')[0]
+	#method = flow.request.path.split('/')[3].split('?')[0]
 
-    #if method == 'getjson':
+	#if method == 'getjson':
        #flow.request.path=flow.request.path.replace(method,"getxml")
             
 
